17461d5449cd512070c7acd4998fcc6d34814ea4
jsimonrichard <jsimonrichard@gmail.com>
2021-07-06 01:48:40 +0000

    Update main to output generated at 8e74f1a

---
+++ pins.h
@@ -0,0 +1,20 @@
+#ifndef PINS_HEADER
+#define PINS_HEADER
+
+#define BUTTON_PIN 1
+#define BUZZER_PIN 18
+
+#define IRS1_PIN 4
+#define IRS2_PIN 9
+#define IRS3_PIN 13
+#define IRS4_PIN 14
+
+#define MOTOR_L_VREF 17
+#define MOTOR_L_RIN 22
+#define MOTOR_L_FIN 21
+
+#define MOTOR_R_VREF 18
+#define MOTOR_R_RIN 20
+#define MOTOR_R_FIN 19
+
+#endif
